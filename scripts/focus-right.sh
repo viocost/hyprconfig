c188b067dee9f849739858fca91e0ed69bd055e8
Konstantin Y. Rybakov <viocost@gmail.com>
2026-02-14 02:28:50 +0200

    focus scripts

---
+++ scripts/focus-right.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ACTIVE_WINDOW_JSON=$(hyprctl activewindow -j)
+
+FOCUSED_PID=$(echo "$ACTIVE_WINDOW_JSON" | jq -r '.pid')
+WINDOW_TITLE=$(echo "$ACTIVE_WINDOW_JSON" | jq -r '.title')
+
+send_nvim_command() {
+  NVIM_SERVER=$(ps -o command= -p "$FOCUSED_PID" | grep -oP 'NVIM_LISTEN_ADDRESS=\K\S+')
+  if [ -n "$NVIM_SERVER" ]; then
+    nvim --server "$NVIM_SERVER" --remote-send "<C-w>h"
+    exit 0
+  fi
+}
+
+if [[ $WINDOW_TITLE == *"nvim!"* ]]; then
+  send_nvim_command
+  exit 0
+fi
+
+if [[ $WINDOW_TITLE == *"tmux"* ]]; then
+  if tmux display-message -p '#{pane_at_right}' | grep -q 0; then
+    tmux select-pane -R
+    exit 0
+  fi
+fi
+
+hyprctl dispatch movefocus r
